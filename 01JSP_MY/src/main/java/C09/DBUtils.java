d701afe1a35b5499b2073aa23acf82d192815b1c
JwebD12 <gkdlfnzo12@naver.com>
2025-04-04 17:24:51 +0900

    Day005 2025-04-04 jsp C09 DB연습

--- 01JSP_MY/src/main/java/C09/DBUtils.java
+++ 01JSP_MY/src/main/java/C09/DBUtils.java
@@ -4,7 +4,8 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
-import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
 
 import C04.UserDto;
 
@@ -37,12 +38,52 @@ public class DBUtils {
 	public int insertUser(UserDto userDto) throws Exception {
 		pstmt = conn.prepareStatement("insert into tbl_user values(?,?,?)");
 		pstmt.setString(1, userDto.getUserid());
-		pstmt.setString(1, userDto.getPassword());
-		pstmt.setString(1, userDto.getRole());
+		pstmt.setString(2, userDto.getPassword());
+		pstmt.setString(3, userDto.getRole());
 		int result = pstmt.executeUpdate();
 		
+		conn.commit();
 		pstmt.close();
 		return result;
+	}
+	public List<UserDto> selectAllUser() throws Exception{
+		pstmt = conn.prepareStatement("select * from tbl_user");
+		rs = pstmt.executeQuery();
+		
+		List<UserDto> list = new ArrayList();	//리턴값 저장용 새로운 객체
+		UserDto userDto = null;	//
+		if(rs!=null) {
+			while(rs.next()) {
+				userDto = new UserDto();
+				userDto.setUserid(rs.getString("userid"));
+				userDto.setPassword(rs.getString("password"));
+				userDto.setRole(rs.getString("role"));
+				list.add(userDto);
+			}
+			
+		}
+		rs.close();
+		pstmt.close();
+		return list;
+		
+	}
+	
+	public UserDto selectOneUser(String userid) throws Exception{
+		pstmt = conn.prepareStatement("select * from tbl_user where userid=?");
+		pstmt.setString(1, userid);
+		rs = pstmt.executeQuery();
+		UserDto userDto = null;	//
+		if(rs!=null) {
+			rs.next();
+			userDto = new UserDto();
+			userDto.setUserid(rs.getString("userid"));
+			userDto.setPassword(rs.getString("password"));
+			userDto.setRole(rs.getString("role"));
+							
+		}
+		rs.close();
+		pstmt.close();
+		return userDto;
 	}
 
 }
